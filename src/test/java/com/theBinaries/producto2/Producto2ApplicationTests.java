4280851911ecb154f490305decd3843e6f79a132
pau <papau84@gmail.com>
2021-10-06 23:55:17 +0200

    Ldap funcionando
    springLdap funcionando perfectamente

---
+++ src/test/java/com/theBinaries/producto2/Producto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theBinaries.producto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Producto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
