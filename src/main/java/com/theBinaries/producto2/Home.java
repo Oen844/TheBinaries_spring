d88422ebd0dadcfeee43907ea674ea4e84539d4e
pau <papau84@gmail.com>
2021-10-07 18:41:57 +0200

    mejorando

--- src/main/java/com/theBinaries/producto2/Home.java
+++ src/main/java/com/theBinaries/producto2/Home.java
@@ -2,6 +2,7 @@ package com.theBinaries.producto2;
 
 import org.springframework.stereotype.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
