2cfec72728a57dfd8941816022d25da750882cc5
pau <papau84@gmail.com>
2021-10-09 18:28:39 +0200

    Controller jsp
    controller y jsp funcionando

--- src/main/java/com/theBinaries/producto2/controllers/HomeController.java
+++ src/main/java/com/theBinaries/producto2/controllers/HomeController.java
@@ -1,14 +1,8 @@
-package com.sopa.controllers;
-
-
-
+package com.theBinaries.producto2.controllers;
 
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
 
 
 @Controller
